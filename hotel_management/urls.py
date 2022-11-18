012f9abaea4bf0d69c22be6ce622bd1a40336fe0
ahmadreza tahmasbi <ahmadreza.tmb@gmail.com>
2022-11-18 21:02:41 +0330

    add image upload url

--- hotel_management/urls.py
+++ hotel_management/urls.py
@@ -1,9 +1,21 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt import views as jwt_views
-from .views import Hotel, HotelViewSet
+from .views import Hotel, HotelRetrieveUpdateDestroyView, RoomRetrieveUpdateDestroyView, RoomListCreateView, \
+    HotelListCreateView, HotelRoomsListView, HotelRoomImageCreateView
 from authentication.views import UserCreationView
-app_name = 'hotel_management'
-router = DefaultRouter()
-router.register(r'', HotelViewSet, basename="")
-urlpatterns = router.urls
+
+
+
+urlpatterns = [
+    # Hotel urls
+    path('<int:pk>/', HotelRetrieveUpdateDestroyView.as_view()),
+    path('', HotelListCreateView.as_view()),
+    path('<int:pk>/room/', HotelRoomsListView.as_view()),
+
+    # Room urls
+    path('room/', RoomListCreateView.as_view()),
+    path('room/<int:pk>/', RoomRetrieveUpdateDestroyView.as_view()),
+    path('room/image/', HotelRoomImageCreateView.as_view())
+]
+
