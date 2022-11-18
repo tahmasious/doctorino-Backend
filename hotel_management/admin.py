53f21a0964042a27be4c3cd28f2f5735acd7877c
ahmadreza tahmasbi <ahmadreza.tmb@gmail.com>
2022-11-18 21:00:14 +0330

    change hotel and hotel room model name

--- hotel_management/admin.py
+++ hotel_management/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from hotel_management.models import Hotel
+from hotel_management.models import Hotel, Room, RoomImage
 
 admin.site.register(Hotel)
+admin.site.register(Room)
+admin.site.register(RoomImage)
