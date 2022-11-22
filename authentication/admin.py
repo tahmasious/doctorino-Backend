42fb34edeb39b60d32ca76285033f80899ea2565
ahmadreza tahmasbi <ahmadreza.tmb@gmail.com>
2022-11-22 15:13:37 +0330

    add Hotelowner to admin

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from authentication.models import Doctor, User
+from authentication.models import Doctor, User, HotelOwner
 
 admin.site.register(User)
 admin.site.register(Doctor)
+admin.site.register(HotelOwner)
