fd33b42133cfc37b90d4fa0e89a10c5b6767547f
Amirhossein mirmohammadi <Seyyed AmirHossein Mrmohammadi>
2022-11-23 00:49:50 +0330

    urls updated

--- doctor_management/urls.py
+++ doctor_management/urls.py
@@ -4,9 +4,16 @@ from rest_framework_simplejwt import views as jwt_views
 from .views import DoctorViewSet
 from authentication.views import UserCreationView
 from rest_framework import routers
+from .views import DoctorRetrieveUpdateDestroyView, SpecialityListView
 
 app_name = 'doctor_management'
 
 router = routers.SimpleRouter()
 router.register('', DoctorViewSet)
-urlpatterns = router.urls
+
+
+urlpatterns = [
+    path('<int:pk>/', DoctorRetrieveUpdateDestroyView.as_view()),
+    path('', include(router.urls)),
+    path('specialities/', SpecialityListView.as_view()),
+]
