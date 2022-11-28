f3b7f5a9ce89385712d10a300576ac5d47fab216
Amirhossein mirmohammadi <Seyyed AmirHossein Mrmohammadi>
2022-11-28 19:14:26 +0330

    adding necessary fields to doctor model

--- doctor_management/models.py
+++ doctor_management/models.py
@@ -26,6 +26,9 @@ class Doctor(models.Model):
     city = models.CharField(max_length=50, blank=False, null=True)
     clinic_address = models.CharField(max_length=250, blank=True, null=True)
     image = models.ImageField(upload_to='doctor-image', blank=True, null=True)
+    phone_number = models.CharField(max_length=11, blank=False, null=True)
+    office_number = models.CharField(max_length=11, blank=False, null=True)
+    education = models.CharField(max_length=50, blank=False, null=True)
 
     def __str__(self):
         return self.user.first_name + " " + self.user.last_name
