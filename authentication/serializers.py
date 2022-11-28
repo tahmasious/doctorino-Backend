00b4641835a4968734ec72ad18f5afaf703d2b8f
Amirhossein mirmohammadi <Seyyed AmirHossein Mrmohammadi>
2022-11-28 18:26:28 +0330

    user creation bug fixed

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -11,10 +11,10 @@ class UserSerializer(serializers.ModelSerializer):
         fields = "__all__"
 
     def create(self, validated_data):
-        new_user = User.objects.create(email=validated_data['email'])
-        new_user.set_password(validated_data['password'])
-        new_user.save()
-        return new_user
+        user = User.objects.create(**validated_data)
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
 
 
 class UserListSerializer(serializers.ModelSerializer):
