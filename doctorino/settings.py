b4e639c402ea70849e1298417e5b64e18c52da5b
ahmadreza <ahmadreza.tmb@gmail.com>
2022-11-30 20:11:28 +0330

    change db to postgresql

--- doctorino/settings.py
+++ doctorino/settings.py
@@ -89,8 +89,12 @@ WSGI_APPLICATION = 'doctorino.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'drinodb',  # DATA BASE NAME
+        'USER': 'drino',  # DATA BASE USER NAME
+        'PASSWORD': 'D@CT@R1N0',  # USER PASSWORD
+        'HOST': '127.0.0.1',  # DON'T CHANGE
+        'PORT': '5432',  # DON'T CHANGE
     }
 }
 
