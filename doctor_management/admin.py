892723c08b140a5ee141edec203321e279d947d1
ahmadreza <ahmadreza.tmb@gmail.com>
2022-12-05 18:13:01 +0330

    add work day model to admin page

--- doctor_management/admin.py
+++ doctor_management/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Doctor, Specialty
+from .models import Doctor, Specialty, WorkDayPeriod
 
 admin.site.register(Doctor)
 admin.site.register(Specialty)
+admin.site.register(WorkDayPeriod)
