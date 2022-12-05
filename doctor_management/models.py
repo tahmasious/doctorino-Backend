afa1c499a95212743438364a8c17c2baebd7d813
ahmadreza <ahmadreza.tmb@gmail.com>
2022-12-05 18:13:36 +0330

    add WorkDayPeriod model

--- doctor_management/models.py
+++ doctor_management/models.py
@@ -2,12 +2,14 @@ from django.db import models
 from authentication.models import User
 from django.contrib.gis.db import models as lcmodels
 from django.contrib.gis.geos import Point
+from django.core.exceptions import ValidationError
 
 class Specialty(models.Model):
     name = models.CharField(max_length=250, unique=True, blank=False, null=False)
 
     def __str__(self):
         return self.name
+
 
 class Doctor(models.Model):
     GENDER_CHOICES = (
@@ -31,6 +33,34 @@ class Doctor(models.Model):
     office_number = models.CharField(max_length=11, blank=False, null=True)
     education = models.CharField(max_length=50, blank=False, null=True)
     location = lcmodels.PointField(geography=True, default=Point(0.0, 0.0))
+    description = models.TextField(max_length=7000, null=True, blank=True)
 
     def __str__(self):
         return self.user.first_name + " " + self.user.last_name
+
+
+class WorkDayPeriod(models.Model):
+    WEEK_DAY_CHOICES = (
+        (0, "شنبه"),
+        (1, "یک شنبه"),
+        (2, "دو شنبه"),
+        (3, "سه شنبه"),
+        (4, "چهارشنبه"),
+        (5, "پنج شنبه"),
+        (6, "جمعه"),
+    )
+
+    doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE)
+    day = models.IntegerField(choices=WEEK_DAY_CHOICES)
+    from_time = models.TimeField()
+    to_time = models.TimeField()
+
+    def clean(self):
+        if self.from_time > self.to_time :
+            raise ValidationError(
+                {"from_time": "زمان شروع دوره نباید بعد از زمان پایان دوره کاری باشد."}
+            )
+
+    def save(self, *args, **kwargs):
+        self.full_clean()
+        return super().save(*args, **kwargs)
