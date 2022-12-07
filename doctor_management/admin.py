8dbfde03e0801b0831e8e0ca202345387990ab13
ahmadreza <ahmadreza.tmb@gmail.com>
2022-12-07 15:19:29 +0330

    add model to admin panel

--- doctor_management/admin.py
+++ doctor_management/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Doctor, Specialty, WorkDayPeriod
+from .models import Doctor, Specialty, WorkDayPeriod, Appointment
 
 admin.site.register(Doctor)
 admin.site.register(Specialty)
 admin.site.register(WorkDayPeriod)
+admin.site.register(Appointment)
