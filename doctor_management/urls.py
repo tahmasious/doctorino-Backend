e993b7c4facc65f7828b4c6b1f68929901364bf6
ahmadreza <ahmadreza.tmb@gmail.com>
2022-12-07 15:20:50 +0330

    add appointment endpoints

--- doctor_management/urls.py
+++ doctor_management/urls.py
@@ -5,12 +5,17 @@ from authentication.views import UserCreationView
 from rest_framework import routers
 from .views import (DoctorListView, DoctorCreateView,
                     DoctorRetrieveUpdateDestroyView, SpecialtyListView,
-                    user_id_to_doctor_id, DoctorSearchByLocationSpecialty, WorkDayPeriodModelViewSet)
+                    user_id_to_doctor_id, DoctorSearchByLocationSpecialty, WorkDayPeriodModelViewSet,
+                    AppointmentModelViewSet, DoctorAllWorkDaysListView, DoctorAllAppointmentsInProfilePageListView,
+                    DetailedDoctorAllAppointmentsListView)
 
 
 app_name = 'doctor_management'
-router = DefaultRouter()
-router.register(r'', WorkDayPeriodModelViewSet ,basename="workday")
+workday_router = DefaultRouter()
+workday_router.register(r'', WorkDayPeriodModelViewSet, basename="workday")
+
+appointment_router = DefaultRouter()
+appointment_router.register(r'', AppointmentModelViewSet, basename='appointment')
 
 urlpatterns = [
     path('<int:pk>/', DoctorRetrieveUpdateDestroyView.as_view()),
@@ -25,5 +30,11 @@ urlpatterns = [
     path('search/', DoctorSearchByLocationSpecialty.as_view()),
 
     # work day period times endpoint
-    path('workday/', include(router.urls))
+    path('workday/', include(workday_router.urls)),
+    path('<int:pk>/workday/', DoctorAllWorkDaysListView.as_view()),  # all workdays of a specific doctor
+
+    # appointment endpoints
+    path('appointment/', include(appointment_router.urls)),  # new - delete - update - retrieve - list
+    path('<int:pk>/appointment/', DoctorAllAppointmentsInProfilePageListView.as_view()),  # all appointments of specific doctor for profile page
+    path('<int:pk>/appointment/detailed/', DetailedDoctorAllAppointmentsListView.as_view())  #  all appointments of specific doctor for doctor panel
 ]
