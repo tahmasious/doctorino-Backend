cd4ce775d97e2bc239737f4f5ac97c2066abbf8b
ahmadreza <ahmadreza.tmb@gmail.com>
2022-12-15 12:51:50 +0330

    change Hotel admin view

--- hotel_management/admin.py
+++ hotel_management/admin.py
@@ -1,8 +1,19 @@
 from django.contrib import admin
-from hotel_management.models import Hotel, Room, RoomImage, Feature, HotelReview
+from hotel_management.models import Hotel, Room, RoomImage, Feature, HotelReview, HotelImage
 
-admin.site.register(Hotel)
+
+class HotelAdmin(admin.ModelAdmin):
+    list_display = ("hotel_owner", "trade_code", "hotel_name", "hotel_stars", "is_active")
+    list_filter = ("hotel_owner", "hotel_name")
+
+    class Meta:
+        model = Hotel
+
+admin.site.register(Hotel, HotelAdmin)
+
+
 admin.site.register(Room)
 admin.site.register(RoomImage)
 admin.site.register(Feature)
 admin.site.register(HotelReview)
+admin.site.register(HotelImage)
