11f4cc3a939e7ef9451f58ef1d9fbc9cb964428a
ahmadreza <ahmadreza.tmb@gmail.com>
2022-12-19 20:21:20 +0330

    addtional fields for hotel owner

--- authentication/models.py
+++ authentication/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-
+from django_jalali.db import models as jmodels
 
 class User(AbstractUser):
     username = models.CharField(max_length=120, unique=True, blank=False, null=True)
@@ -13,5 +13,21 @@ class User(AbstractUser):
 
 
 class HotelOwner(models.Model):
+    GENDER_CHOICES = (
+        (0, 'male'),
+        (1, 'female'),
+        (2, 'not specified'),
+    )
+
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='owner')
     shaba_code = models.CharField(max_length=256, null=True, blank=True)
+    father_name = models.CharField(max_length=124, null=True, blank=False)
+    national_code = models.CharField(max_length=10, unique=True, blank=False, null=True)
+    social_number = models.CharField(max_length=20, blank=False, null=True)
+    gender = models.IntegerField(choices=GENDER_CHOICES, default=2)
+    first_phone_number = models.CharField(max_length=12, blank=False, null=True)
+    second_phone_number = models.CharField(max_length=12, blank=True, null=True)
+    area_code = models.CharField(max_length=20, blank=True, null=True)
+    telephone_number = models.CharField(max_length=20, blank=True, null=True)
+    address = models.TextField(blank=True, null=True)
+    birth_day = jmodels.jDateField(blank=False, null=True)
