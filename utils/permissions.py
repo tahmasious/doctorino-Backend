7289f493b6959f34f5eb62a4a151a37aba95e417
ahmadreza <ahmadreza.tmb@gmail.com>
2022-12-26 23:17:01 +0330

    add doctor permission classes

--- utils/permissions.py
+++ utils/permissions.py
@@ -1,5 +1,38 @@
 from rest_framework import permissions
 
+class IsAppointmentOwnerOrReadOnly(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return obj.doctor.user == request.user
+
+
+class IsWorkDayOwnerOrReadOnly(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return obj.doctor.user == request.user
+
+
+class IsRoomOwnerOrReadOnly(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return obj.hotel.hotel_owner.user == request.user
+
+
+class IsHotelReserveOwnerOrReadOnly(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return obj.hotel_room.hotel.hotel_owner.user == request.user
+
+class IsOwnerOrReadOnly(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return obj.user == request.user
+
 
 class IsHotelOwnerOrReadOnly(permissions.BasePermission):
     """
