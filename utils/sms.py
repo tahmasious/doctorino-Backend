a3e738d4d6a2d5c458d1dafe121e259b2172e940
ahmadreza <ahmadreza.tmb@gmail.com>
2023-01-06 17:10:22 +0330

    sms send debug

--- utils/sms.py
+++ utils/sms.py
@@ -4,19 +4,20 @@ import requests
 
 
 def send_sms(text, extra_num=None):
-    url = 'https://api.payamak-panel.com/post/Send.asmx/SendSimpleSMS'
+    url = 'http://api.payamak-panel.com/post/Send.asmx/SendByBaseNumber2'
     username = '09912377076'
     password = 'Ahmadreza12@'
     headers = {'Content-Type': 'application/x-www-form-urlencoded'}
     data = {
         'username': username,
         'password': password,
-        'from': '50004001377076',
-        'to': ['09912377076', '09172948570', extra_num],
+        'to': '09912377076',
         'text': text,
-        'isflash': False
+        'bodyId': 56886
     }
     new_URL = url
     new_headers = {'Content-type': 'application/x-www-form-urlencoded', 'Accept': 'application/json'}
+    response = requests.post(new_URL, data=(data), headers=new_headers)
+    data['to'] = '09172948570'
     response = requests.post(new_URL, data=(data), headers=new_headers)
     return response.content
